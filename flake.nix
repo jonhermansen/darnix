4abcbdeb4cad25063bbd87ad58f0f70655ec9a43
Jon Hermansen <jon.hermansen@obsidian.systems>
2026-05-19 13:26:14 -0400

    flake: switch local inputs to GitHub for portability

--- flake.nix
+++ flake.nix
@@ -6,15 +6,15 @@
     flake-utils.url = "github:numtide/flake-utils";
 
     # Versions taken from apple-oss-distributions/distribution-macOS@rel/macOS-26 release.json
-    xnu-src                  = { url = "git+file:///Users/user/work/xnu"; flake = false; };
+    xnu-src                  = { url = "github:jonhermansen/xnu/nix"; flake = false; };
     bootstrap_cmds-src       = { url = "github:apple-oss-distributions/bootstrap_cmds/bootstrap_cmds-138"; flake = false; };
     dtrace-src               = { url = "github:apple-oss-distributions/dtrace/dtrace-413"; flake = false; };
     AvailabilityVersions-src = { url = "github:apple-oss-distributions/AvailabilityVersions/AvailabilityVersions-157.2"; flake = false; };
     Libsystem-src            = { url = "github:apple-oss-distributions/Libsystem/Libsystem-1356"; flake = false; };
     libplatform-src          = { url = "github:apple-oss-distributions/libplatform/libplatform-375.100.10"; flake = false; };
     libdispatch-src          = { url = "github:apple-oss-distributions/libdispatch/libdispatch-1542.100.32"; flake = false; };
-    grub-src                 = { url = "git+file:///Users/user/work/grub"; };
-    hfs-src                  = { url = "git+file:///Users/user/work/hfs"; flake = false; };
+    grub-src                 = { url = "github:jonhermansen/grub/nix"; };
+    hfs-src                  = { url = "github:jonhermansen/hfs/nix"; flake = false; };
   };
 
   outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
